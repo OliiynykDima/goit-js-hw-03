73f84011614276967227a989bac8aa4a46b8d565
OliiynykDima <d.oliynyk2007kiev@gmail.com>
2020-10-08 10:48:24 +0300

    v1

---
+++ task-4.js
@@ -0,0 +1,30 @@
+const countTotalSalary = function (employees) {
+    let totalSalary = 0;
+    for(const employee in employees){
+        const salary = employees[employee];
+        totalSalary += salary;
+    }
+
+    return totalSalary;
+};
+
+/*
+ * Вызовы функции для проверки работоспособности твоей реализации.
+ */
+console.log(countTotalSalary({})); // 0
+
+console.log(
+    countTotalSalary({
+        mango: 100,
+        poly: 150,
+        alfred: 80,
+    }),
+); // 330
+
+console.log(
+    countTotalSalary({
+        kiwi: 200,
+        lux: 50,
+        chelsy: 150,
+    }),
+); // 400
